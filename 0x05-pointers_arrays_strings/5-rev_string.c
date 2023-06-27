a6634a5cedbb1b0ef0585cc1766d6eace701206c
Bashir Umar Idris <bashtrust15@gmail.com>
2023-06-27 13:54:47 -0700

    Add mandatory tasks files

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+
+/**
+ * rev_string - reverse a string.
+ *
+ *@s: the string to be reversed.
+ */
+
+void rev_string(char *s)
+{
+	int length, k, i;
+	char l1, l2;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+
+	k = length - 1;
+	i = 0;
+
+	while (k > i)
+	{
+		l1 = s[i];
+		l2 = s[k];
+		s[i] = l2;
+		s[k] = l1;
+		k--;
+		i++;
+	}
+}
