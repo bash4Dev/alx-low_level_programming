8ed58e36df6bdad62ad13ca1a67c418f4120b8bd
Bashir Umar Idris <idrisbashirumr@gmail.com>
2023-10-20 22:13:01 -0700

    fix bug

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_N
+
 #include <elf.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -8,10 +9,12 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-/** EOF header functions*/
+
+/*EOF header functions*/
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
